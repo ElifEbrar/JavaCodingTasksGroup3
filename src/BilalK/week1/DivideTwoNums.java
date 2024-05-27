371e048704f467cc17f0fde9b6ce02b14e0338fb
bilal <bilal.koca@gmail.com>
2024-05-27 19:30:53 +0300

    by bilalK

---
+++ src/BilalK/week1/DivideTwoNums.java
@@ -0,0 +1,22 @@
+package BilalK.week1;
+
+public class DivideTwoNums {
+
+    public static void main(String[] args) {
+
+    int a= 45;
+    int b= 5;
+
+    divideMethod(a, b);
+
+    }
+public static void divideMethod(int a, int b) {
+
+  System.out.println(Math.floorDiv(a,b));
+}
+
+}
+
+/*
+Write a method that can divide two numbers without using division operator
+ */
