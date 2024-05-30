61a0200b74507527972c22a5aaa897431d37ef6e
dikicier <dikicier@gmail.com>
2024-05-30 13:53:40 +0300

    week01 tasks completed

---
+++ src/Berkay/week01/numbersOddorEven.java
@@ -0,0 +1,20 @@
+package Berkay.week01;
+
+public class numbersOddorEven {
+    public static void main(String[] args) {
+
+        System.out.println(oddorEven(23));
+        String sonuc= oddorEven(24);
+        System.out.println("sonuc = " + sonuc);
+
+    }
+
+    public static String oddorEven (int num){
+        String result="";
+        if (num%2==0){
+            result="Even";
+        }else result="Odd";
+
+       return result;
+    }
+}
