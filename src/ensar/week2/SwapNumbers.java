86b7adb145921946f6c24f8baaccc1960fcf4db8
ensarxguler <ensarguler64@gmail.com>
2024-06-05 00:37:54 +0300

    Week-1&2 Tasks

---
+++ src/ensar/week2/SwapNumbers.java
@@ -0,0 +1,13 @@
+package ensar.week2;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int num1 = 1, num2 = 10;
+        
+        num1 = num2 - num1;
+        num2 = num2 - num1;
+        num1 = num1 + num2;
+
+        System.out.println(num1 + " | " + num2);        
+    }
+}
