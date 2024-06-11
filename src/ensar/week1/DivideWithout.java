622eabd77556240bfff99e34c50bb89bcdbd9f35
ensarxguler <ensarguler64@gmail.com>
2024-06-11 18:35:11 +0300

    Week-3 Tasks

--- src/ensar/week1/DivideWithout.java
+++ src/ensar/week1/DivideWithout.java
@@ -1,13 +1,17 @@
 package ensar.week1;
 
 public class DivideWithout {
+    
     public static void main(String[] args) {
+        
         withoutDivisionOperator(10, 10);
         withoutDivisionOperator(10, 0);
         withoutDivisionOperator(0, 0);
+        
     }
     
     static void withoutDivisionOperator(int dividend, int divider) {
+        
         int counter = 0;
         int container = 0;
         for (int i = 0; i < dividend; i++) {
@@ -19,5 +23,7 @@ public class DivideWithout {
         }
 
         System.out.println(divider == 0 ? (dividend == 0 ? "Error" : "Infinity") : counter);
+        
     }
+    
 }
--- src/ensar/week1/OddOrEven.java
+++ src/ensar/week1/OddOrEven.java
@@ -1,13 +1,17 @@
 package ensar.week1;
 
 public class OddOrEven {
+    
     public static void main(String[] args) {
+        
         System.out.println(identify(0));
         System.out.println(identify(1));
+        
     }
     
     static String identify(int num) {
         String result = "";
         return num % 2 == 0 ? result = "Even" : "Odd";
     }
+    
 }
