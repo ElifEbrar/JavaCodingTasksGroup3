48677a2899482fa3377503d2e89c8e554c9c4ffd
bilal <bilal.koca@gmail.com>
2024-06-18 20:31:11 +0300

    1- by bilalKoca

--- src/BilalK/week4/FrequencyOfCharacters.java
+++ src/BilalK/week4/FrequencyOfCharacters.java
@@ -6,6 +6,7 @@ public class FrequencyOfCharacters {
 Write a return method that can find the frequency of characters
   Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
      */
+
     public static void main(String[] args) {
         String str = "AAABBCDD";
 
