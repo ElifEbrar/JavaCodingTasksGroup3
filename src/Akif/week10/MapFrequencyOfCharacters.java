033d9d10ff582a889af9616646f8202d3faa5932
MAS-Akif <akoxako568@gmail.com>
2024-09-11 21:01:50 +0300

    Week10

---
+++ src/Akif/week10/MapFrequencyOfCharacters.java
@@ -0,0 +1,18 @@
+package Akif.week10;
+import java.util.Map;
+import java.util.HashMap;
+public class MapFrequencyOfCharacters {
+    public static void main(String[] args) {
+        String input = "hello world";
+        printCharFrequency(input);
+
+    }public static void printCharFrequency(String str) {
+        Map<Character, Integer> freqMap = new HashMap<>();
+
+        for (char c : str.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+
+        freqMap.forEach((k, v) -> System.out.println(k + ": " + v));
+    }
+}
